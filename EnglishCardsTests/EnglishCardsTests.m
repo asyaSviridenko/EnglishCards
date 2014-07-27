939d4526e15d8ff9e378153cb21260b609a55b3b
asya <asya.kononova@gmail.com>
2014-07-27 17:34:25 +0400

    Initial Commit

---
+++ EnglishCardsTests/EnglishCardsTests.m
@@ -0,0 +1,34 @@
+//
+//  EnglishCardsTests.m
+//  EnglishCardsTests
+//
+//  Created by asya on 27/07/14.
+//  Copyright (c) 2014 asya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EnglishCardsTests : XCTestCase
+
+@end
+
+@implementation EnglishCardsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
